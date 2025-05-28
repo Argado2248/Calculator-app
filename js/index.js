5732d1be9b4249d7454a0b52b1043b7fb2ae363f
Argado2248 <oliver.bngtssn@gmail.com>
2025-05-28 15:23:17 +0200

    Made buttons 4-9 work as well

--- js/index.js
+++ js/index.js
@@ -18,6 +18,42 @@ buttonSelector3.addEventListener("click", function(){
     console.log(numberOutput);
 });
 
+const buttonSelector4 = document.getElementById("btnFour"); //Samma som tidigare
+buttonSelector4.addEventListener("click", function(){
+    numberOutput += 4
+    console.log(numberOutput);
+});
+
+const buttonSelector5 = document.getElementById("btnFive"); //Samma som tidigare
+buttonSelector5.addEventListener("click", function(){
+    numberOutput += 5
+    console.log(numberOutput);
+});
+
+const buttonSelector6 = document.getElementById("btnSix"); //Samma som tidigare
+buttonSelector6.addEventListener("click", function(){
+    numberOutput += 6
+    console.log(numberOutput);
+});
+
+const buttonSelector7 = document.getElementById("btnSeven"); //Samma som tidigare
+buttonSelector7.addEventListener("click", function(){
+    numberOutput += 7
+    console.log(numberOutput);
+});
+
+const buttonSelector8 = document.getElementById("btnEight"); //Samma som tidigare
+buttonSelector8.addEventListener("click", function(){
+    numberOutput += 8
+    console.log(numberOutput);
+});
+
+const buttonSelector9 = document.getElementById("btnNine"); //Samma som tidigare
+buttonSelector9.addEventListener("click", function(){
+    numberOutput += 9
+    console.log(numberOutput);
+});
+
 const buttonSelectorAdd = document.getElementById("btnAdd"); 
 buttonSelectorAdd.addEventListener("click", function(){
     numberOutput += "+"; //Sätter + så att jag kan använda "eval" som kommer senare. 
